1dc367675584b431458f755b4b66e30da0d6ed7e
Lam Haoyin <root@eatswap.org>
2022-02-19 18:21:30 +0800

    fix: support expressions like "+1++2"

--- Week1/ConsoleCalculator/Program.cs
+++ Week1/ConsoleCalculator/Program.cs
@@ -4,7 +4,7 @@ namespace ConsoleCalculator;
 
 public class ConsoleCalculatorEntry {
 	public static void Main() {
-		var expressionMatching = new Regex("\\s{0,}(?<numA>-{0,1}[0-9\\.]{1,})\\s{0,}(?<op>[+\\-*\\/%^&|])\\s{0,}(?<numB>-{0,1}[0-9\\.]{1,})");
+		var expressionMatching = new Regex("\\s{0,}(?<numA>[\\-+]{0,1}[0-9\\.]{1,})\\s{0,}(?<op>[+\\-*\\/%^&|])\\s{0,}(?<numB>[\\-+]{0,1}[0-9\\.]{1,})");
 
 		Console.Write("Type your expression, then press <Enter>: ");
 		var line = Console.ReadLine();
