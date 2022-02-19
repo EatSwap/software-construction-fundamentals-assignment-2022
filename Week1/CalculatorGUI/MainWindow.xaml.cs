a1a43b7c2d431d7d0cb1b3c816353ba9e5ae831b
Lam Haoyin <root@eatswap.org>
2022-02-19 20:19:00 +0800

    add: implementation of GUI calc

--- Week1/CalculatorGUI/MainWindow.xaml.cs
+++ Week1/CalculatorGUI/MainWindow.xaml.cs
@@ -1,25 +1,86 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace CalculatorGUI {
-	/// <summary>
-	/// Interaction logic for MainWindow.xaml
-	/// </summary>
-	public partial class MainWindow : Window {
-		public MainWindow() {
-			InitializeComponent();
+namespace CalculatorGUI; 
+
+public partial class MainWindow : Window {
+	public MainWindow() {
+		InitializeComponent();
+	}
+
+	private void DoCalculate(object sender, RoutedEventArgs e) {
+		var numA = FirstOperand.Text;
+		var numB = SecondOperand.Text;
+		var op = OperatorSelector.Text;
+
+		// Test for null operator
+		if (string.IsNullOrEmpty(op)) {
+			MessageBox.Show("Please select an operator", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+			return;
 		}
+		
+		// Test for empty operands
+		if (string.IsNullOrEmpty(numA) || string.IsNullOrEmpty(numB)) {
+			MessageBox.Show("Please enter both operands", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+			return;
+		}
+		
+		string result;
+		try {
+			result = Calculate(numA, numB, op[0]);
+		} catch (Exception ex) {
+			MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+			return;
+		}
+		
+		Result.Text = result;
+	}
+
+	private static string Calculate(string numA, string numB, char operation) {
+		if (!double.TryParse(numA, out var dA) | !double.TryParse(numB, out var dB)) throw new Exception("At least one operand could not be parsed as valid number.");
+		var isFloatingPointArithmetic = numA.Contains('.') || numB.Contains('.') || Math.Abs(dA) > long.MaxValue || Math.Abs(dB) > long.MaxValue;
+		isFloatingPointArithmetic |= !long.TryParse(numA, out var lA) | !long.TryParse(numB, out var lB);
+
+		switch (operation) {
+		case '+':
+			if (isFloatingPointArithmetic)
+				return "" + (dA + dB);
+			else
+				return "" + (lA + lB);
+		case '-':
+			if (isFloatingPointArithmetic)
+				return "" + (dA - dB);
+			else
+				return "" + (lA - lB);
+		case '*':
+			if (isFloatingPointArithmetic)
+				return "" + dA * dB;
+			else
+				return "" + lA * lB;
+		case '/':
+			return "" + dA / dB;
+		case '^':
+			if (isFloatingPointArithmetic)
+				throw new Exception("Invalid operation xor on floating-point number");
+			else
+				return "" + (lA ^ lB);
+		case '&':
+			if (isFloatingPointArithmetic)
+				throw new Exception("Invalid operation bit-and on floating-point number");
+			else
+				return "" + (lA & lB);
+		case '%':
+			if (isFloatingPointArithmetic)
+				return "" + dA % dB;
+			else
+				return "" + lA % lB;
+		case '|':
+			if (isFloatingPointArithmetic)
+				throw new Exception("Invalid operation bit-or on floating-point number");
+			else
+				return "" + (lA | lB);
+		}
+
+		throw new Exception("code considered unreachable reached");
 	}
 }
