6715fc26499427d8e7217cbe4f71e4566165f100
Lam Haoyin <root@eatswap.org>
2022-02-19 13:46:11 +0800

    fix: bit-or operation, duplicate parse

--- Week1/ConsoleCalculator/Program.cs
+++ Week1/ConsoleCalculator/Program.cs
@@ -3,7 +3,7 @@
 namespace ConsoleCalculator {
 	public class ConsoleCalculatorEntry {
 		public static void Main() {
-			var expressionMatching = new Regex("\\s{0,}(?<numA>-{0,1}[0-9\\.]{1,})\\s{0,}(?<op>[+\\-*\\/%^&])\\s{0,}(?<numB>-{0,1}[0-9\\.]{1,})");
+			var expressionMatching = new Regex("\\s{0,}(?<numA>-{0,1}[0-9\\.]{1,})\\s{0,}(?<op>[+\\-*\\/%^&|])\\s{0,}(?<numB>-{0,1}[0-9\\.]{1,})");
 			
 			Console.Write("Type your expression, then press <Enter>: ");
 			var line = Console.ReadLine();
@@ -22,59 +22,61 @@ namespace ConsoleCalculator {
 			var numB = regexMatch.Groups["numB"].Value;
 			var op = regexMatch.Groups["op"].Value;
 
-			bool isFloatingPointArithmetic = numA.Contains('.') || numB.Contains('.');
+			double dA = double.Parse(numA), dB = double.Parse(numB);
+			bool isFloatingPointArithmetic = numA.Contains('.') || numB.Contains('.') || Math.Abs(dA) > long.MaxValue || Math.Abs(dB) > long.MaxValue;
+			long lA = isFloatingPointArithmetic ? 0 : long.Parse(numA), lB = isFloatingPointArithmetic ? 0 : long.Parse(numB);
 			
 			switch (op[0]) {
 				case '+':
 					if (isFloatingPointArithmetic) {
-						Console.WriteLine(double.Parse(numA) + double.Parse(numB));
+						Console.WriteLine(dA + dB);
 					} else {
-						Console.WriteLine(long.Parse(numA) + long.Parse(numB));
+						Console.WriteLine(lA + lB);
 					}
 					break;
 				case '-':
 					if (isFloatingPointArithmetic) {
-						Console.WriteLine(double.Parse(numA) - double.Parse(numB));
+						Console.WriteLine(dA - dB);
 					} else {
-						Console.WriteLine(long.Parse(numA) - long.Parse(numB));
+						Console.WriteLine(lA - lB);
 					}
 					break;
 				case '*':
 					if (isFloatingPointArithmetic) {
-						Console.WriteLine(double.Parse(numA) * double.Parse(numB));
+						Console.WriteLine(dA * dB);
 					} else {
-						Console.WriteLine(long.Parse(numA) * long.Parse(numB));
+						Console.WriteLine(lA * lB);
 					}
 					break;
 				case '/':
-					Console.WriteLine(double.Parse(numA) / double.Parse(numB));
+					Console.WriteLine(dA / dB);
 					break;
 				case '^':
 					if (isFloatingPointArithmetic) {
 						Console.WriteLine("Invalid operation xor on floating-point number");
 					} else {
-						Console.WriteLine(long.Parse(numA) ^ long.Parse(numB));
+						Console.WriteLine(lA ^ lB);
 					}
 					break;
 				case '&':
 					if (isFloatingPointArithmetic) {
 						Console.WriteLine("Invalid operation bit-and on floating-point number");
 					} else {
-						Console.WriteLine(long.Parse(numA) & long.Parse(numB));
+						Console.WriteLine(lA & lB);
 					}
 					break;
 				case '%':
 					if (isFloatingPointArithmetic) {
-						Console.WriteLine(double.Parse(numA) % double.Parse(numB));
+						Console.WriteLine(dA % dB);
 					} else {
-						Console.WriteLine(long.Parse(numA) % long.Parse(numB));
+						Console.WriteLine(lA % lB);
 					}
 					break;
 				case '|':
 					if (isFloatingPointArithmetic) {
 						Console.WriteLine("Invalid operation bit-or on floating-point number");
 					} else {
-						Console.WriteLine(long.Parse(numA) | long.Parse(numB));
+						Console.WriteLine(lA | lB);
 					}
 					break;
 				default:
