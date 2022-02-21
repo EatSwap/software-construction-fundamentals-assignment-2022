bff044f05603a34745a2f444ffcad302555f7caf
Lam Haoyin <root@eatswap.org>
2022-02-21 12:37:50 +0800

    add: button logic

--- Week1/WindowsFormsCalculator/Form1.Designer.cs
+++ Week1/WindowsFormsCalculator/Form1.Designer.cs
@@ -24,24 +24,24 @@
 		/// the contents of this method with the code editor.
 		/// </summary>
 		private void InitializeComponent() {
-			this.ds = new System.Windows.Forms.Label();
+			this.Banner = new System.Windows.Forms.Label();
 			this.OperandFirst = new System.Windows.Forms.TextBox();
 			this.OperatorSelector = new System.Windows.Forms.ComboBox();
 			this.OperandSecond = new System.Windows.Forms.TextBox();
-			this.Calculate = new System.Windows.Forms.Button();
+			this.CalculateButton = new System.Windows.Forms.Button();
 			this.Result = new System.Windows.Forms.Label();
 			this.SuspendLayout();
 			// 
-			// ds
+			// Banner
 			// 
-			this.ds.Anchor = ((System.Windows.Forms.AnchorStyles) ((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right)));
-			this.ds.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
-			this.ds.Location = new System.Drawing.Point(68, 30);
-			this.ds.Margin = new System.Windows.Forms.Padding(0);
-			this.ds.Name = "ds";
-			this.ds.Size = new System.Drawing.Size(475, 55);
-			this.ds.TabIndex = 0;
-			this.ds.Text = "Enter your favourite operand:";
+			this.Banner.Anchor = ((System.Windows.Forms.AnchorStyles) ((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right)));
+			this.Banner.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+			this.Banner.Location = new System.Drawing.Point(68, 30);
+			this.Banner.Margin = new System.Windows.Forms.Padding(0);
+			this.Banner.Name = "Banner";
+			this.Banner.Size = new System.Drawing.Size(475, 55);
+			this.Banner.TabIndex = 0;
+			this.Banner.Text = "Enter your favourite operand:";
 			// 
 			// OperandFirst
 			// 
@@ -71,15 +71,16 @@
 			this.OperandSecond.Size = new System.Drawing.Size(200, 31);
 			this.OperandSecond.TabIndex = 3;
 			// 
-			// Calculate
+			// CalculateButton
 			// 
-			this.Calculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
-			this.Calculate.Location = new System.Drawing.Point(240, 150);
-			this.Calculate.Name = "Calculate";
-			this.Calculate.Size = new System.Drawing.Size(120, 40);
-			this.Calculate.TabIndex = 4;
-			this.Calculate.Text = "Calculate";
-			this.Calculate.UseVisualStyleBackColor = true;
+			this.CalculateButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+			this.CalculateButton.Location = new System.Drawing.Point(240, 150);
+			this.CalculateButton.Name = "CalculateButton";
+			this.CalculateButton.Size = new System.Drawing.Size(120, 40);
+			this.CalculateButton.TabIndex = 4;
+			this.CalculateButton.Text = "Calculate";
+			this.CalculateButton.UseVisualStyleBackColor = true;
+			this.CalculateButton.Click += new System.EventHandler(this.Calculate_Click);
 			// 
 			// Result
 			// 
@@ -97,11 +98,11 @@
 			this.BackColor = System.Drawing.SystemColors.Control;
 			this.ClientSize = new System.Drawing.Size(594, 271);
 			this.Controls.Add(this.Result);
-			this.Controls.Add(this.Calculate);
+			this.Controls.Add(this.CalculateButton);
 			this.Controls.Add(this.OperandSecond);
 			this.Controls.Add(this.OperatorSelector);
 			this.Controls.Add(this.OperandFirst);
-			this.Controls.Add(this.ds);
+			this.Controls.Add(this.Banner);
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
 			this.MaximumSize = new System.Drawing.Size(600, 300);
 			this.MinimumSize = new System.Drawing.Size(600, 300);
@@ -113,7 +114,7 @@
 
 		private System.Windows.Forms.Label Result;
 
-		private System.Windows.Forms.Button Calculate;
+		private System.Windows.Forms.Button CalculateButton;
 
 		private System.Windows.Forms.TextBox OperandSecond;
 
@@ -121,7 +122,7 @@
 
 		private System.Windows.Forms.TextBox OperandFirst;
 
-		private System.Windows.Forms.Label ds;
+		private System.Windows.Forms.Label Banner;
 
 		#endregion
 	}
--- Week1/WindowsFormsCalculator/Form1.cs
+++ Week1/WindowsFormsCalculator/Form1.cs
@@ -1,17 +1,86 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace WindowsFormsCalculator {
 	public partial class Form1 : Form {
 		public Form1() {
 			InitializeComponent();
+		}
+
+		private static string Calculate(string numA, string numB, char operation) {
+        		if (!double.TryParse(numA, out var dA) | !double.TryParse(numB, out var dB)) throw new Exception("At least one operand could not be parsed as valid number.");
+        		var isFloatingPointArithmetic = numA.Contains('.') || numB.Contains('.') || Math.Abs(dA) > long.MaxValue || Math.Abs(dB) > long.MaxValue;
+        		isFloatingPointArithmetic |= !long.TryParse(numA, out var lA) | !long.TryParse(numB, out var lB);
+        
+        		switch (operation) {
+        		case '+':
+        			if (isFloatingPointArithmetic)
+        				return "" + (dA + dB);
+        			else
+        				return "" + (lA + lB);
+        		case '-':
+        			if (isFloatingPointArithmetic)
+        				return "" + (dA - dB);
+        			else
+        				return "" + (lA - lB);
+        		case '*':
+        			if (isFloatingPointArithmetic)
+        				return "" + dA * dB;
+        			else
+        				return "" + lA * lB;
+        		case '/':
+        			return "" + dA / dB;
+        		case '^':
+        			if (isFloatingPointArithmetic)
+        				throw new Exception("Invalid operation xor on floating-point number");
+        			else
+        				return "" + (lA ^ lB);
+        		case '&':
+        			if (isFloatingPointArithmetic)
+        				throw new Exception("Invalid operation bit-and on floating-point number");
+        			else
+        				return "" + (lA & lB);
+        		case '%':
+        			if (isFloatingPointArithmetic)
+        				return "" + dA % dB;
+        			else
+        				return "" + lA % lB;
+        		case '|':
+        			if (isFloatingPointArithmetic)
+        				throw new Exception("Invalid operation bit-or on floating-point number");
+        			else
+        				return "" + (lA | lB);
+        		}
+        
+        		throw new Exception("code considered unreachable reached");
+        	}
+
+		private void Calculate_Click(object sender, EventArgs e) {
+			string numA = OperandFirst.Text, numB = OperandSecond.Text;
+			string op = OperatorSelector.Text;
+			
+			// Test for null operator
+			if (string.IsNullOrEmpty(op)) {
+				MessageBox.Show("Please select an operator.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				return;
+			}
+		
+			// Test for empty operands
+			if (string.IsNullOrEmpty(numA) || string.IsNullOrEmpty(numB)) {
+				MessageBox.Show("Please enter both operands.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				return;
+			}
+		
+			string result;
+			try {
+				result = Calculate(numA, numB, op[0]);
+			} catch (Exception ex) {
+				MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				return;
+			}
+		
+			Result.Text = result;
 		}
 	}
 }
