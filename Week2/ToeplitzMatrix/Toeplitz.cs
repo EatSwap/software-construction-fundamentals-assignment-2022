3e1bf0fa23fabee2a78ec9c0abc97ab570eb98e4
Lam Haoyin <root@eatswap.org>
2022-02-25 10:31:20 +0800

    feat: simplify (2)

--- Week2/ToeplitzMatrix/Toeplitz.cs
+++ Week2/ToeplitzMatrix/Toeplitz.cs
@@ -2,28 +2,24 @@
 
 public class Toeplitz {
 	public static bool IsToeplitz(int[][] matrix) {
-		if (matrix.Length == 0 || matrix[0].Length == 0) {
+		if (matrix.Length == 0 || matrix[0].Length == 0)
 			return false;
-		} else if (matrix.Length == 1) {
+		else if (matrix.Length == 1)
 			return true;
-		}
 
 		int m = matrix.Length, n = matrix[0].Length;
 
-		for (int i = 0; i < m; ++i) {
-			if (matrix[i].Length != n) {
+		for (int i = 0; i < m; ++i)
+			if (matrix[i].Length != n)
 				throw new Exception("argument is not a matrix");
-			}
-		}
 		
 		int k = m < n ? m : n, lim = m < n ? n - m : m - n;
 		
 		for (int i = 0; i <= lim; ++i) {
 			var baseLine = matrix[m < n ? 0 : i][m < n ? i : 0];
 			for (int j = 1; j < k; ++j) {
-				if (matrix[(m < n ? 0 : i) + j][(m < n ? i : 0) + j] != baseLine) {
+				if (matrix[(m < n ? 0 : i) + j][(m < n ? i : 0) + j] != baseLine)
 					return false;
-				}
 			}
 		}
 
