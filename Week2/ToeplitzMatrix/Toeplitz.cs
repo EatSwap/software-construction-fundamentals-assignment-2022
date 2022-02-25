15108be09cbf7f03f020dca9ed60a93fc6cde4cb
Lam Haoyin <root@eatswap.org>
2022-02-25 10:45:31 +0800

    fix: misunderstanding of Toeplitz (2)

--- Week2/ToeplitzMatrix/Toeplitz.cs
+++ Week2/ToeplitzMatrix/Toeplitz.cs
@@ -2,26 +2,35 @@
 
 public class Toeplitz {
 	public static bool IsToeplitz(int[][] matrix) {
-		if (matrix.Length == 0 || matrix[0].Length == 0)
+		if (matrix.Length == 0 || matrix[0].Length == 0) {
 			return false;
-		else if (matrix.Length == 1)
+		} else if (matrix.Length == 1) {
 			return true;
+		}
 
 		int m = matrix.Length, n = matrix[0].Length;
 
-		for (int i = 0; i < m; ++i)
-			if (matrix[i].Length != n)
-				throw new Exception("argument is not a matrix");
-		
-		int k = m < n ? m : n, lim = m < n ? n - m : m - n;
-		
-		for (int i = 0; i <= lim; ++i) {
-			var baseLine = matrix[m < n ? 0 : i][m < n ? i : 0];
-			for (int j = 1; j < k; ++j) {
-				if (matrix[(m < n ? 0 : i) + j][(m < n ? i : 0) + j] != baseLine)
+		// Does not check if x, y are valid
+		var verdict = (int x, int y) => {
+			int baseLine = matrix[x][y];
+			for (int i = 0; i + x < m && i + y < n; ++i)
+				if (matrix[i + x][i + y] != baseLine)
 					return false;
-			}
-		}
+			return true;
+		};
+
+		if (!verdict(0, 0))
+			return false;
+		
+		// In x direction
+		for (int i = 1; i < n; ++i)
+			if (!verdict(0, i))
+				return false;
+
+		// In y direction
+		for (int i = 1; i < m; ++i)
+			if (!verdict(i, 0))
+				return false;
 
 		return true;
 	}
