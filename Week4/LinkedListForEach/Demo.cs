fef8cc78c755d480b78070e46732ed74fcdc8410
Lam Haoyin <root@eatswap.org>
2022-03-11 09:57:33 +0800

    add: LinkedListForEach

---
+++ Week4/LinkedListForEach/Demo.cs
@@ -0,0 +1,17 @@
+﻿namespace LinkedListForEach;
+
+public class Demo {
+	public static void Main() {
+		var stringList = new GenericList<string>();
+		
+		stringList.Add("gugu");
+		stringList.Add("guagua");
+		stringList.Add("guagu");
+		stringList.Add("guaguagu");
+		stringList.Add("guaguaguagu");
+
+		foreach (var str in stringList) {
+			Console.WriteLine(str);
+		}
+	}
+}
---
+++ Week4/LinkedListForEach/GenericList.cs
@@ -0,0 +1,49 @@
+namespace LinkedListForEach;
+
+using System.Collections;
+
+public class Node<T> {
+	public Node<T>? Next { get; set; }
+	public T Data { get; set; }
+
+	public Node(T t) {
+		Data = t;
+		Next = null;
+	}
+}
+
+public class GenericList<T> : IEnumerable<T> {
+	private Node<T>? head;
+	
+	private Node<T>? tail;
+	
+	public GenericList() {
+		tail = head = null;
+	}
+
+	public Node<T> Head {
+		get => head;
+	}
+
+	public void Add(T t) {
+		Node<T> n = new Node<T>(t);
+		if (tail == null) {
+			head = tail = n;
+		} else {
+			tail.Next = n;
+			tail = n;
+		}
+	}
+	
+	public IEnumerator<T> GetEnumerator() {
+		Node<T>? ptr = head;
+		while (ptr != null) {
+			yield return ptr.Data;
+			ptr = ptr.Next;
+		}
+	}
+	
+	IEnumerator IEnumerable.GetEnumerator() {
+		return GetEnumerator();
+	}
+}
