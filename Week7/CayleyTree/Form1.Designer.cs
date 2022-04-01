272442ece787823208ed2d20b4fb2c1dabc878c3
Lam Haoyin <root@eatswap.org>
2022-04-01 22:50:32 +0800

    add: finish basic functions

--- Week7/CayleyTree/Form1.Designer.cs
+++ Week7/CayleyTree/Form1.Designer.cs
@@ -19,7 +19,7 @@
             base.Dispose(disposing);
         }
 
-        #region G
+        #region Windows Form Designer Generated Code
 
         /// <summary>
         /// Required method for Designer support - do not modify
@@ -28,15 +28,28 @@
         private void InitializeComponent() {
             this.mainLayoutTable = new System.Windows.Forms.TableLayoutPanel();
             this.drawingPanel = new System.Windows.Forms.Panel();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.labelTh2 = new System.Windows.Forms.Label();
             this.textBoxStemLength = new System.Windows.Forms.TextBox();
             this.labelLength = new System.Windows.Forms.Label();
             this.remakeButton = new System.Windows.Forms.Button();
             this.labelRecursionDepth = new System.Windows.Forms.Label();
             this.textBoxRecursionDepth = new System.Windows.Forms.TextBox();
             this.labelPer1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBoxPer1 = new System.Windows.Forms.TextBox();
+            this.labelPer2 = new System.Windows.Forms.Label();
+            this.textBoxPer2 = new System.Windows.Forms.TextBox();
+            this.labelTh1 = new System.Windows.Forms.Label();
+            this.textBoxTh1 = new System.Windows.Forms.TextBox();
+            this.textBoxTh2 = new System.Windows.Forms.TextBox();
+            this.labelPenColour = new System.Windows.Forms.Label();
+            this.comboBoxPenColour = new System.Windows.Forms.ComboBox();
+            this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
             this.mainLayoutTable.SuspendLayout();
+            this.drawingPanel.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
             this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -57,6 +70,7 @@
             // 
             // drawingPanel
             // 
+            this.drawingPanel.Controls.Add(this.statusStrip1);
             this.drawingPanel.Dock = System.Windows.Forms.DockStyle.Fill;
             this.drawingPanel.Location = new System.Drawing.Point(210, 3);
             this.drawingPanel.Name = "drawingPanel";
@@ -64,18 +78,43 @@
             this.drawingPanel.TabIndex = 0;
             this.drawingPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.drawingPanel_Paint);
             // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripProgressBar1,
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 393);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(480, 22);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(109, 17);
+            this.toolStripStatusLabel1.Text = "Nothing drawn yet.";
+            // 
             // tableLayoutPanel1
             // 
             this.tableLayoutPanel1.ColumnCount = 2;
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.labelTh2, 0, 5);
             this.tableLayoutPanel1.Controls.Add(this.textBoxStemLength, 1, 1);
             this.tableLayoutPanel1.Controls.Add(this.labelLength, 0, 1);
             this.tableLayoutPanel1.Controls.Add(this.remakeButton, 0, 7);
             this.tableLayoutPanel1.Controls.Add(this.labelRecursionDepth, 0, 0);
             this.tableLayoutPanel1.Controls.Add(this.textBoxRecursionDepth, 1, 0);
             this.tableLayoutPanel1.Controls.Add(this.labelPer1, 0, 2);
-            this.tableLayoutPanel1.Controls.Add(this.textBox1, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.textBoxPer1, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.labelPer2, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.textBoxPer2, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.labelTh1, 0, 4);
+            this.tableLayoutPanel1.Controls.Add(this.textBoxTh1, 1, 4);
+            this.tableLayoutPanel1.Controls.Add(this.textBoxTh2, 1, 5);
+            this.tableLayoutPanel1.Controls.Add(this.labelPenColour, 0, 6);
+            this.tableLayoutPanel1.Controls.Add(this.comboBoxPenColour, 1, 6);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 3);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
@@ -91,11 +130,21 @@
             this.tableLayoutPanel1.Size = new System.Drawing.Size(201, 415);
             this.tableLayoutPanel1.TabIndex = 1;
             // 
+            // labelTh2
+            // 
+            this.labelTh2.AutoSize = true;
+            this.labelTh2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelTh2.Location = new System.Drawing.Point(3, 205);
+            this.labelTh2.Name = "labelTh2";
+            this.labelTh2.Size = new System.Drawing.Size(94, 41);
+            this.labelTh2.TabIndex = 11;
+            this.labelTh2.Text = "左分支角度";
+            this.labelTh2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
             // textBoxStemLength
             // 
             this.textBoxStemLength.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.textBoxStemLength.Location = new System.Drawing.Point(103, 51);
-            this.textBoxStemLength.MaxLength = 3;
             this.textBoxStemLength.Name = "textBoxStemLength";
             this.textBoxStemLength.Size = new System.Drawing.Size(95, 20);
             this.textBoxStemLength.TabIndex = 4;
@@ -163,13 +212,232 @@
             this.labelPer1.Text = "右分支长度比";
             this.labelPer1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
-            // textBox1
+            // textBoxPer1
             // 
-            this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.Left;
-            this.textBox1.Location = new System.Drawing.Point(103, 92);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(95, 20);
-            this.textBox1.TabIndex = 6;
+            this.textBoxPer1.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.textBoxPer1.Location = new System.Drawing.Point(103, 92);
+            this.textBoxPer1.Name = "textBoxPer1";
+            this.textBoxPer1.Size = new System.Drawing.Size(95, 20);
+            this.textBoxPer1.TabIndex = 6;
+            this.textBoxPer1.Text = "0.6";
+            // 
+            // labelPer2
+            // 
+            this.labelPer2.AutoSize = true;
+            this.labelPer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelPer2.Location = new System.Drawing.Point(3, 123);
+            this.labelPer2.Name = "labelPer2";
+            this.labelPer2.Size = new System.Drawing.Size(94, 41);
+            this.labelPer2.TabIndex = 7;
+            this.labelPer2.Text = "左分支长度比";
+            this.labelPer2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // textBoxPer2
+            // 
+            this.textBoxPer2.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.textBoxPer2.Location = new System.Drawing.Point(103, 133);
+            this.textBoxPer2.Name = "textBoxPer2";
+            this.textBoxPer2.Size = new System.Drawing.Size(95, 20);
+            this.textBoxPer2.TabIndex = 8;
+            this.textBoxPer2.Text = "0.7";
+            // 
+            // labelTh1
+            // 
+            this.labelTh1.AutoSize = true;
+            this.labelTh1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelTh1.Location = new System.Drawing.Point(3, 164);
+            this.labelTh1.Name = "labelTh1";
+            this.labelTh1.Size = new System.Drawing.Size(94, 41);
+            this.labelTh1.TabIndex = 9;
+            this.labelTh1.Text = "右分支角度";
+            this.labelTh1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // textBoxTh1
+            // 
+            this.textBoxTh1.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.textBoxTh1.Location = new System.Drawing.Point(103, 174);
+            this.textBoxTh1.Name = "textBoxTh1";
+            this.textBoxTh1.Size = new System.Drawing.Size(95, 20);
+            this.textBoxTh1.TabIndex = 10;
+            this.textBoxTh1.Text = "30";
+            // 
+            // textBoxTh2
+            // 
+            this.textBoxTh2.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.textBoxTh2.Location = new System.Drawing.Point(103, 215);
+            this.textBoxTh2.Name = "textBoxTh2";
+            this.textBoxTh2.Size = new System.Drawing.Size(95, 20);
+            this.textBoxTh2.TabIndex = 12;
+            this.textBoxTh2.Text = "20";
+            // 
+            // labelPenColour
+            // 
+            this.labelPenColour.AutoSize = true;
+            this.labelPenColour.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelPenColour.Location = new System.Drawing.Point(3, 246);
+            this.labelPenColour.Name = "labelPenColour";
+            this.labelPenColour.Size = new System.Drawing.Size(94, 41);
+            this.labelPenColour.TabIndex = 13;
+            this.labelPenColour.Text = "画笔颜色";
+            this.labelPenColour.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // comboBoxPenColour
+            // 
+            this.comboBoxPenColour.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.comboBoxPenColour.DisplayMember = "1";
+            this.comboBoxPenColour.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxPenColour.FormattingEnabled = true;
+            this.comboBoxPenColour.Items.AddRange(new object[] {
+            "AliceBlue",
+            "AntiqueWhite",
+            "Aqua",
+            "Aquamarine",
+            "Azure",
+            "Beige",
+            "Bisque",
+            "Black",
+            "BlanchedAlmond",
+            "Blue",
+            "BlueViolet",
+            "Brown",
+            "BurlyWood",
+            "CadetBlue",
+            "Chartreuse",
+            "Chocolate",
+            "Coral",
+            "CornflowerBlue",
+            "Cornsilk",
+            "Crimson",
+            "Cyan",
+            "DarkBlue",
+            "DarkCyan",
+            "DarkGoldenrod",
+            "DarkGray",
+            "DarkGreen",
+            "DarkKhaki",
+            "DarkMagenta",
+            "DarkOliveGreen",
+            "DarkOrange",
+            "DarkOrchid",
+            "DarkRed",
+            "DarkSalmon",
+            "DarkSeaGreen",
+            "DarkSlateBlue",
+            "DarkSlateGray",
+            "DarkTurquoise",
+            "DarkViolet",
+            "DeepPink",
+            "DeepSkyBlue",
+            "DimGray",
+            "DodgerBlue",
+            "Firebrick",
+            "FloralWhite",
+            "ForestGreen",
+            "Fuchsia",
+            "Gainsboro",
+            "GhostWhite",
+            "Gold",
+            "Goldenrod",
+            "Gray",
+            "Green",
+            "GreenYellow",
+            "Honeydew",
+            "HotPink",
+            "IndianRed",
+            "Indigo",
+            "Ivory",
+            "Khaki",
+            "Lavender",
+            "LavenderBlush",
+            "LawnGreen",
+            "LemonChiffon",
+            "LightBlue",
+            "LightCoral",
+            "LightCyan",
+            "LightGoldenrodYellow",
+            "LightGray",
+            "LightGreen",
+            "LightPink",
+            "LightSalmon",
+            "LightSeaGreen",
+            "LightSkyBlue",
+            "LightSlateGray",
+            "LightSteelBlue",
+            "LightYellow",
+            "Lime",
+            "LimeGreen",
+            "Linen",
+            "Magenta",
+            "Maroon",
+            "MediumAquamarine",
+            "MediumBlue",
+            "MediumOrchid",
+            "MediumPurple",
+            "MediumSeaGreen",
+            "MediumSlateBlue",
+            "MediumSpringGreen",
+            "MediumTurquoise",
+            "MediumVioletRed",
+            "MidnightBlue",
+            "MintCream",
+            "MistyRose",
+            "Moccasin",
+            "NavajoWhite",
+            "Navy",
+            "OldLace",
+            "Olive",
+            "OliveDrab",
+            "Orange",
+            "OrangeRed",
+            "Orchid",
+            "PaleGoldenrod",
+            "PaleGreen",
+            "PaleTurquoise",
+            "PaleVioletRed",
+            "PapayaWhip",
+            "PeachPuff",
+            "Peru",
+            "Pink",
+            "Plum",
+            "PowderBlue",
+            "Purple",
+            "RebeccaPurple",
+            "Red",
+            "RosyBrown",
+            "RoyalBlue",
+            "SaddleBrown",
+            "Salmon",
+            "SandyBrown",
+            "SeaGreen",
+            "SeaShell",
+            "Sienna",
+            "Silver",
+            "SkyBlue",
+            "SlateBlue",
+            "SlateGray",
+            "Snow",
+            "SpringGreen",
+            "SteelBlue",
+            "Tan",
+            "Teal",
+            "Thistle",
+            "Tomato",
+            "Turquoise",
+            "Violet",
+            "Wheat",
+            "White",
+            "WhiteSmoke",
+            "Yellow",
+            "YellowGreen"});
+            this.comboBoxPenColour.Location = new System.Drawing.Point(103, 256);
+            this.comboBoxPenColour.Name = "comboBoxPenColour";
+            this.comboBoxPenColour.Size = new System.Drawing.Size(95, 21);
+            this.comboBoxPenColour.TabIndex = 14;
+            // 
+            // toolStripProgressBar1
+            // 
+            this.toolStripProgressBar1.Name = "toolStripProgressBar1";
+            this.toolStripProgressBar1.Size = new System.Drawing.Size(100, 16);
             // 
             // Form1
             // 
@@ -182,6 +450,10 @@
             this.Text = "Cayley Tree";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.mainLayoutTable.ResumeLayout(false);
+            this.drawingPanel.ResumeLayout(false);
+            this.drawingPanel.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
@@ -192,14 +464,25 @@
 
         private System.Windows.Forms.TableLayoutPanel mainLayoutTable;
         private System.Windows.Forms.Panel drawingPanel;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label labelTh2;
+        private System.Windows.Forms.TextBox textBoxStemLength;
+        private System.Windows.Forms.Label labelLength;
         private System.Windows.Forms.Button remakeButton;
         private System.Windows.Forms.Label labelRecursionDepth;
         private System.Windows.Forms.TextBox textBoxRecursionDepth;
-        private System.Windows.Forms.TextBox textBoxStemLength;
-        private System.Windows.Forms.Label labelLength;
         private System.Windows.Forms.Label labelPer1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBoxPer1;
+        private System.Windows.Forms.Label labelPer2;
+        private System.Windows.Forms.TextBox textBoxPer2;
+        private System.Windows.Forms.Label labelTh1;
+        private System.Windows.Forms.TextBox textBoxTh1;
+        private System.Windows.Forms.TextBox textBoxTh2;
+        private System.Windows.Forms.Label labelPenColour;
+        private System.Windows.Forms.ComboBox comboBoxPenColour;
+        private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar1;
     }
 }
 
--- Week7/CayleyTree/Form1.cs
+++ Week7/CayleyTree/Form1.cs
@@ -1,24 +1,39 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Drawing;
 using System.Windows.Forms;
 
 namespace CayleyTree {
 	public partial class Form1 : Form {
-		private readonly double per1 = 0.6;
-		private readonly double per2 = 0.7;
-		private readonly double th1 = 30 * Math.PI / 180;
-		private readonly double th2 = 20 * Math.PI / 180;
+		private double per1 = 0.6;
+		private double per2 = 0.7;
+
+		private double th1 = 30 * Math.PI / 180;
+		private double th2 = 20 * Math.PI / 180;
+
 		private int n = 11;
+
 		private double stemLength = 100.0;
-		private Graphics graphics;
+
+		private Pen pen = new Pen(Color.Black);
+
+		private bool canPaint = false;
+
+		private int pivotX = 200;
+		private int pivotY = 310;
 
 		public Form1() {
 			InitializeComponent();
 			AutoScaleBaseSize = new Size(6, 140);
 			ClientSize = new Size(600, 300);
 		}
+
 		private void Form1_Load(object sender, EventArgs e) {
-			// Does nothing.
+			// Pre-define the pen colour <- black.
+			comboBoxPenColour.SelectedIndex = 7;
+
+			// Progress bar step
+			toolStripProgressBar1.Step = 1;
 		}
 
 		private static void showErrorMessage(string message) {
@@ -26,7 +41,7 @@ namespace CayleyTree {
 		}
 
 		private static double tryParseDouble(string str, string componentName, double maxRange) {
-			if (double.TryParse(str, out double d)) {
+			if (double.TryParse(str, out double d) && d <= maxRange) {
 				return d;
 			} else {
 				showErrorMessage($"{componentName} 的输入须在 [0, {maxRange}] 范围内！");
@@ -34,36 +49,50 @@ namespace CayleyTree {
 			}
 		}
 
-		private void drawCayleyTree(int n, double x0, double y0, double stemLen, double th) {
-			if (n == 0) return;
-
-			var x1 = x0 + stemLen * Math.Cos(th);
-			var y1 = y0 + stemLen * Math.Sin(th);
-
-			drawLine(x0, y0, x1, y1);
-
-			drawCayleyTree(n - 1, x1, y1, per1 * stemLen, th + th1);
-			drawCayleyTree(n - 1, x1, y1, per2 * stemLen, th - th2);
-		}
-
-		private void drawLine(double x0, double y0, double x1, double y1) {
-			graphics.DrawLine(Pens.Blue, (int) x0, (int) y0, (int) x1, (int) y1);
-        }
-
         private void drawingPanel_Paint(object sender, PaintEventArgs e) {
-			this.graphics = e.Graphics;
-			drawCayleyTree(n, 200, 310, stemLength, -Math.PI / 2);
+			if (!canPaint) return;
+
+			toolStripProgressBar1.Maximum = (1 << n) - 1;
+
+			Action<int, double, double, double, double> drawCayleyTree = null;
+			drawCayleyTree = (n, x0, y0, stemLen, th) => {
+				if (n == 0) return;
+				var x1 = x0 + stemLen * Math.Cos(th);
+				var y1 = y0 + stemLen * Math.Sin(th);
+
+				e.Graphics.DrawLine(pen, (int) x0, (int) y0, (int) x1, (int) y1);
+
+				toolStripProgressBar1.PerformStep();
+
+				drawCayleyTree(n - 1, x1, y1, per1 * stemLen, th + th1);
+				drawCayleyTree(n - 1, x1, y1, per2 * stemLen, th - th2);
+			};
+
+			var stopWatch = new Stopwatch();
+			stopWatch.Start();
+			drawCayleyTree(n, pivotX, pivotY, this.stemLength, -Math.PI / 2);
+			stopWatch.Stop();
+
+			toolStripStatusLabel1.Text = $"Total steps: {(1 << n) - 1}, Time Elapsed: {stopWatch.ElapsedMilliseconds} ms";
 		}
 
         private void remakeButton_Click(object sender, EventArgs e) {
 			// Try parsing recursion depth
 			try {
 				n = (int) tryParseDouble(textBoxRecursionDepth.Text, "递归深度", 30);
-				stemLength = (int) tryParseDouble(textBoxStemLength.Text, "主干长度", 300);
+				stemLength = tryParseDouble(textBoxStemLength.Text, "主干长度", 300);
+				per1 = tryParseDouble(textBoxPer1.Text, "右分支长度比", 1.0);
+				per2 = tryParseDouble(textBoxPer2.Text, "左分支长度比", 1.0);
+				th1 = (Math.PI / 180) * tryParseDouble(textBoxTh1.Text, "右分支角度", 180.0);
+				th2 = (Math.PI / 180) * tryParseDouble(textBoxTh2.Text, "左分支角度", 180.0);
+				pen = new Pen(Color.FromName(comboBoxPenColour.Text));
 			} catch (ArgumentOutOfRangeException) {
+				canPaint = false;
 				return;
 			}
-
+			canPaint = true;
+			pivotX = drawingPanel.Width / 2;
+			pivotY = drawingPanel.Height;
 			drawingPanel.Refresh();
         }
     }
