3cbf43d1ea0aaa73d28e1b2584264ffca6bf805c
Eat-Swap <root@eatswap.org>
2022-05-09 09:42:57 +0800

    add: various API for CURD

--- Week12/OrderWeb/Controllers/OrderController.cs
+++ Week12/OrderWeb/Controllers/OrderController.cs
@@ -2,6 +2,7 @@
 
 using Base;
 using Microsoft.AspNetCore.Mvc;
+using OrderManageCLI.models;
 using Services;
 
 [ApiController]
@@ -12,5 +13,75 @@ public class OrderController : ControllerBase {
 	[HttpGet("test")]
 	public HttpResponse<string> Test() {
 		return HttpResponse<string>.Success("foo, bar");
+	}
+
+	[HttpPost("")]
+	public HttpResponse<object> CreateOrder(Order order) {
+		try {
+			_orderService.createOrder(order);
+			return HttpResponse<object>.Success();
+		} catch (Exception e) {
+			return HttpResponse<object>.ErrorWithMessage(e.Message);
+		}
+	}
+
+	[HttpGet("{id}")]
+	public HttpResponse<Order> GetOrder(string id) {
+		try {
+			if (!int.TryParse(id, out var orderId)) {
+				throw new Exception("Invalid order id");
+			}
+			var order = _orderService.getOrder(orderId);
+			return HttpResponse<Order>.Success(order);
+		} catch (Exception e) {
+			return HttpResponse<Order>.ErrorWithMessage(e.Message);
+		}
+	}
+
+	[HttpGet("all")]
+	public HttpResponse<List<Order>> GetAllOrders() {
+		try {
+			var orders = _orderService.getAllOrders();
+			return HttpResponse<List<Order>>.Success(orders);
+		} catch (Exception e) {
+			return HttpResponse<List<Order>>.ErrorWithMessage(e.Message);
+		}
+	}
+
+	[HttpPatch("{id}")]
+	public HttpResponse<object> UpdateOrder(string id, Order order) {
+		try {
+			if (!int.TryParse(id, out var orderId)) {
+				throw new Exception("Invalid order id");
+			}
+			_orderService.updateOrder(orderId, order);
+			return HttpResponse<object>.Success(null);
+		}
+		catch (Exception e) {
+			return HttpResponse<object>.ErrorWithMessage(e.Message);
+		}
+	}
+	
+	[HttpPatch("")]
+	public HttpResponse<object> UpdateOrder(Order order) {
+		try {
+			_orderService.updateOrder(order);
+			return HttpResponse<object>.Success(null);
+		} catch (Exception e) {
+			return HttpResponse<object>.ErrorWithMessage(e.Message);
+		}
+	}
+	
+	[HttpDelete("{id}")]
+	public HttpResponse<object> DeleteOrder(string id) {
+		try {
+			if (!int.TryParse(id, out var orderId)) {
+				throw new Exception("Invalid order id");
+			}
+			_orderService.deleteOrder(orderId);
+			return HttpResponse<object>.Success(null);
+		} catch (Exception e) {
+			return HttpResponse<object>.ErrorWithMessage(e.Message);
+		}
 	}
 }
--- Week12/OrderWeb/Services/OrderService.cs
+++ Week12/OrderWeb/Services/OrderService.cs
@@ -1,9 +1,36 @@
 ﻿namespace OrderWeb.Services;
 
+using OrderManageCLI.models;
+
 public sealed class OrderService {
 	// Equivalent Java code:
 	// @Autowired
 	// private OrderService orderService;
 
 	private static readonly OrderManageCLI.OrderService orderService = new();
+	
+	public void createOrder(Order order) {
+		orderService.AddOrder(order);
+	}
+	
+	public void updateOrder(Order order) {
+		orderService.ModifyOrder(order.OrderId, order);
+	}
+	
+	public void updateOrder(long orderId, Order order) {
+		orderService.ModifyOrder(orderId, order);
+	}
+	
+	public void deleteOrder(int orderId) {
+		orderService.DeleteOrder(orderId);
+	}
+	
+	public Order? getOrder(int orderId) {
+		return orderService.FindOrder(orderId);
+	}
+
+	public List<Order> getAllOrders() {
+		return orderService.Find(order => true);
+	}
+	
 }
